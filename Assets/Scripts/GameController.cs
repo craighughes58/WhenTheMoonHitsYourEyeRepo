244e0f2d3866832f14610179c166e59d43383656
MrDoctorb <zane@indybrowns.org>
2024-01-27 10:46:01 -0600

    Squashed commit of the following:
    commit a9d665dd0d4872dff91f21539d5e1f0abe27bd3b
    Author: MrDoctorb <zane@indybrowns.org>
    Date:   Sat Jan 27 10:45:53 2024 -0600
    
        sdghdfhgdhdsfasg

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -27,6 +27,8 @@ public class GameController : MonoBehaviour
 
     #endregion
 
+    public delegate void Alert();
+    public Alert onRoundEnd;
 
     public static GameController Instance;
 
@@ -109,8 +111,9 @@ public class GameController : MonoBehaviour
         //move to player position
 
         //WAIT UNTIL FAILURE
-        yield return new WaitUntil(() => _failedCast);
+        yield return new WaitUntil(() => _failedCast || _successfulCast);
         yield return null;
+        onRoundEnd?.Invoke();
     }
 
 
