722df3d9d3bd5ff1597dc179a8a2bf938388719c
C0ldFu210n <91224821+C0ldFu210n@users.noreply.github.com>
2024-01-27 21:20:15 -0600

    explode star

--- Assets/Scripts/Stars/CrumblingStar.cs
+++ Assets/Scripts/Stars/CrumblingStar.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class CrumblingStar : Star
 {
     public float timeBeforeCrumble = 3f;
+    public float shakeAdder = 1;
 
     private BobberManager _player;
     [Tooltip("The objects that spawn after destruction")]
     [SerializeField] private GameObject _particles;
 
+    public Animator _anim;
+    public float timer;
+
     public override void Hook(BobberManager bobber)
     {
+        _anim = GetComponent<Animator>();
+
         StartCoroutine(Crumble());
     }
 
     private IEnumerator Crumble()
     {
-        yield return new WaitForSeconds(timeBeforeCrumble);
+        timer = 0;
+
+        while (timer < timeBeforeCrumble)
+        {
+            timer += Time.deltaTime;
+
+            _anim.speed = timer * timer + shakeAdder;
+            Debug.Log(_anim.speed);
+
+            yield return null;
+        }
+        //yield return new WaitForSeconds(timeBeforeCrumble);
 
         gc.NotifyCastFailure();
         Instantiate(_particles,transform.position,Quaternion.identity);
