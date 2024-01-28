18f0ef25ba42c11ead0939e4b1c7f7f9fa8b443c
craighughes58 <90798245+craighughes58@users.noreply.github.com>
2024-01-27 20:38:43 -0600

    Squashed commit of the following:
    commit 028c5857c32d698f8b28d692640a4b36d73737f0
    Author: craighughes58 <90798245+craighughes58@users.noreply.github.com>
    Date:   Sat Jan 27 20:37:28 2024 -0600
    
        m ain menus fully functional
    
    commit 654176ae4f145846f0da2148fcde515cf7cf463c
    Merge: 04a202d b7c51c8
    Author: craighughes58 <90798245+craighughes58@users.noreply.github.com>
    Date:   Sat Jan 27 18:08:06 2024 -0600
    
        Merge branch 'Develop' into postProcessing
    
    commit 04a202d22b5d5864fb68c2f2840a7746e02508f6
    Author: craighughes58 <90798245+craighughes58@users.noreply.github.com>
    Date:   Sat Jan 27 18:07:01 2024 -0600
    
        updating menus
    
    commit 36d589c9cd6cda914942ae0d45c4a2264175ea8e
    Author: craighughes58 <90798245+craighughes58@users.noreply.github.com>
    Date:   Sat Jan 27 17:53:13 2024 -0600
    
        deleted
    
    commit 032bc7efa8f64f427f9a37fe91918b99fa4127c5
    Author: craighughes58 <90798245+craighughes58@users.noreply.github.com>
    Date:   Sat Jan 27 17:46:00 2024 -0600
    
        loss scene
    
    commit 2dab9db9c3946768897c827ab8095f6fc0b5dbf9
    Merge: c388e46 c5a4331
    Author: craighughes58 <90798245+craighughes58@users.noreply.github.com>
    Date:   Sat Jan 27 17:38:34 2024 -0600
    
        Merge branch 'Develop' into postProcessing
    
    commit c388e469561eba2bd257fc2c998b6ed058d60287
    Author: craighughes58 <90798245+craighughes58@users.noreply.github.com>
    Date:   Sat Jan 27 17:38:22 2024 -0600
    
        changes to menus

---
+++ Assets/Scripts/UI/FaderBehaviour.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaderBehaviour : MonoBehaviour
+{
+    #region Private Variables
+
+    #region Serialized Variables
+    [Tooltip("The reference to the sprite renderer on the vignette")]
+    [SerializeField] private SpriteRenderer _vignette;
+    [Tooltip("How fast the vignette fades in and out")]
+    [SerializeField] private float _fadingSpeed;
+    [Tooltip("How much darker/lighter the vignette gets over time")]
+    [SerializeField] private float _fadingAmount;
+    [Tooltip("Represents if this is a menu scene")]
+    [SerializeField] private bool _isMenu;
+    [Header("CUTSCENES")]
+    [Tooltip("Represents if this just a scene meant to be watched and then moved on from")]
+    [SerializeField] private bool _isCutScene;
+    [Tooltip("The name of the scene the cutscene switches to")]
+    [SerializeField] private string _sceneAfterCutscene;
+    [Tooltip("How much time the cutscene lasts")]
+    [SerializeField] private float _sceneLength;
+    [Tooltip("The reference to the menu controller in the scene")]
+    [SerializeField] private MainMenuController _menCon;
+
+    #endregion
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!_isMenu)
+        {
+            ChangeVignette();
+        }
+        if (_isCutScene)
+        {
+            StartCoroutine(LeaveCutscene());
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void ChangeVignette()
+    {
+        if (_vignette.color.a <= 0)
+        {
+            StartCoroutine(FadeOut());
+        }
+        else
+        {
+            StartCoroutine(FadeIn());
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator FadeIn()
+    {
+        while (_vignette.color.a > 0)
+        {
+            _vignette.color = new Color(_vignette.color.r, _vignette.color.g, _vignette.color.b, _vignette.color.a - _fadingAmount);
+            yield return new WaitForSeconds(_fadingSpeed);
+        }
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator FadeOut()
+    {
+        while (_vignette.color.a < 1)
+        {
+            _vignette.color = new Color(_vignette.color.r, _vignette.color.g, _vignette.color.b, _vignette.color.a + _fadingAmount);
+            yield return new WaitForSeconds(_fadingSpeed);
+        }
+    }
+
+    private IEnumerator LeaveCutscene()
+    {
+        yield return new WaitForSeconds(_sceneLength);
+        _menCon.MoveScene(_sceneAfterCutscene);
+    }
+}
